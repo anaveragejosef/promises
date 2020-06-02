de26bdde0effbb9a7eeb6d80acae1a66bb8773e0
Johann Lee <jhnlee0526@gmail.com>
2020-06-02 11:43:31 -0700

    "working on basicChaining.js"

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,41 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
+var promiseConstructor = require('./promiseConstructor');
+var promisification = require('./promisification');
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  // Use pluckFirstLine function and pass in readFilePath
+  return promiseConstructor.pluckFirstLineFromFileAsync(readFilePath)
+    /* .then((userName) => {
+      // Check for errors
+      if (!userName) {
+        throw new Error ('No username available');
+      } else {
+        return userName;
+        //return promisification.getGitHubProfileAsync(userName);
+      }
+    }) */
+    // Use .then to pass in gitHubProfile function
+    .then((userName) => {
+      console.log('userName: ', userName);
+      return promisification.getGitHubProfileAsync(userName);
+    })
+    // Use .then to write the response to a file
+    .then((profileData) => {
+      console.log('Profile Data ', profileData); // object
+      console.log('JSON strigify : ', JSON.stringify(profileData)); // json
+      // Return JSON response
+      // return fs.writeFile(writeFilePath, JSON.stringify(profileData));
+      return fs.writeFile(writeFilePath, JSON.stringify(profileData), 'utf8', (err) => {
+        if (err) {
+          throw new Error ('Cannot write to file: ', err);
+        }
+      });
+    })
+    .catch((err) => {
+      console.log('Error while running function: ', err);
+    });
 };
 
 // Export these functions so we can test them
